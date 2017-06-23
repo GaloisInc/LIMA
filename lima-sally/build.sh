c014155e749711f13b4650b6c6404fe9729f006c
Benjamin Jones <bjones@galois.com>
2017-06-23 14:49:00 -0700

    Update build scripts

--- lima-sally/build.sh
+++ lima-sally/build.sh
@@ -4,7 +4,7 @@ cabal sandbox init
 cabal sandbox add-source ../lima
 cabal sandbox add-source ../dependencies/language-sally
 
-cabal configure
+cabal configure --enable-tests
 cabal install --only-dependencies
 cabal build
 
